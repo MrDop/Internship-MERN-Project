8553d39c922c1b459cc2c819eefef6db65618847
sarvesh phase <sarveshphase@gmail.com>
2025-12-22 09:48:43 +0530

    uploaded server.js changes and video.js

--- Project_backend/Server/server.js
+++ Project_backend/Server/server.js
@@ -1,9 +1,26 @@
 const express = require('express');
 
+// Routers
+const userRouter = require('./routes/user');
+const courseRouter = require('./routes/course');
+const videoRouter = require('./routes/video');
+const studentRouter = require('./routes/student');
+
+// Middleware
+const authUser = require('./utils/auth');
+
 const app = express();
 
 app.use(express.json());
 
+// Apply Authentication Globally
+app.use(authUser);
+
+// Mount Routes
+app.use('/user', userRouter);
+app.use('/course', courseRouter);
+app.use('/video', videoRouter);
+app.use('/student', studentRouter);
 
 app.listen(4000, 'localhost', () => {
     console.log('Server is running on port 4000');
